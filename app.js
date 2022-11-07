e5619e9658dbc0057ec328b0052617196c341374
StefanPacola <stevo.pacola@gmail.com>
2022-11-07 11:53:25 +0100

    button functionality added

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const previous = document.querySelector(".previous");
-const next = document.querySelector(".next");
+const prev = document.querySelectorAll(".prev");
+const next = document.querySelectorAll(".next");
 const sections = document.querySelectorAll("section");
 const sectionOne = document.querySelector(".section-1");
 const sectionTwo = document.querySelector(".section-2");
 
-previous.addEventListener("click", () => {
-    if(sectionOne.classList.contains("active")){
-        sections.forEach(function(section){
-            section.classList.remove("active");
-            sectionTwo.classList.add("active")
-        })
-    }
+prev.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(sectionOne.classList.contains("active")){
+            sectionOne.classList.remove("active");
+            sectionTwo.classList.add("active");
+        }
+        else{
+            sectionTwo.classList.remove("active");
+            sectionOne.classList.add("active")
+        }
+    })
 });
 
-next.addEventListener(" click", () => {
-    if(sectionOne.classList.contains("active")){
-        sections.forEach(function(section){
-            section.classList.remove("active");
-            sectionTwo.classList.add("active")
-        })
-    }
+next.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(sectionOne.classList.contains("active")){
+            sectionOne.classList.remove("active");
+            sectionTwo.classList.add("active");
+        }
+        else{
+            sectionTwo.classList.remove("active");
+            sectionOne.classList.add("active")
+        }
+    })
 });
